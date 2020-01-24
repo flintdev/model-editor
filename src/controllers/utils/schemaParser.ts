71ed89f488d82c0df9cb99479b689d186373cdc7
naitianliu <naitianliu@gmail.com>
2020-01-24 12:29:46 -0800

    bug fix - schemaParser.ts to support array field

--- src/controllers/utils/schemaParser.ts
+++ src/controllers/utils/schemaParser.ts
@@ -31,7 +31,7 @@ export class SchemaParser {
                 const data = this.recurToGenerateSchema(children, {});
                 properties[name] = {
                     type,
-                    items: data
+                    ...data
                 };
             }
         }
